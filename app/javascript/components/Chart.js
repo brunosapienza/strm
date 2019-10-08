f0707dad4f3137ba77ec755d3d51b54c68033624
Bruno Sapienza <bruno.sapienza@gmail.com>
2019-10-08 12:50:22 +1100

    modify React component to render the chart based on the query

--- app/javascript/components/Chart.js
+++ app/javascript/components/Chart.js
@@ -5,30 +5,122 @@ import React from "react"
 import PropTypes from "prop-types"
 
 class Chart extends React.Component {
-  data () {
-    return [
-      {
-        "name": "TV",
-        "data": [["time_stamp_1", 10], ["time_stamp_2", 12], ["time_stamp_3", 15]]
-      },
-      {
-        "name": "Radio",
-        "data": [["time_stamp_1", 2], ["time_stamp_2", 4], ["time_stamp_3", 12]]
-      },
-      {
-        "name": "Online",
-        "data": [["time_stamp_1", 20], ["time_stamp_2", 23], ["time_stamp_3", 29]]
-      }
-    ]
+  constructor(props) {
+    super(props);
+    this.state = { query: '', before: '', after: '', interval: '', status: '', data: []};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+    this.resultsPath = this.resultsPath.bind(this);
+  }
+
+  groupByMedium (response) {
+    var groupedByMedium = {};
+
+    response.aggregations.first_agg.buckets.forEach(function (agg_bucket) {
+      var timestamp = agg_bucket.key_as_string;
+
+      agg_bucket.sub_agg.buckets.forEach(function (sub_agg_bucket) {
+        var doc_count = sub_agg_bucket.doc_count
+        var medium = sub_agg_bucket.key
+
+        if(groupedByMedium.hasOwnProperty(medium)){
+          groupedByMedium[medium].push([timestamp, doc_count]);
+        } else {
+          groupedByMedium[medium] = [[timestamp, doc_count]];
+        }
+      });
+    });
+
+    return groupedByMedium;
+  }
+
+  buildChartData (parsedData) {
+    var chartData = [];
+
+    Object.entries(parsedData).forEach(function (entry) {
+      chartData.push({ "name": entry[0], "data": entry[1] });
+    });
+
+    return chartData;
+  }
+
+  handleChange (evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  resultsPath () {
+    const params = {
+      "query": this.state.query,
+      "before": Date.parse(this.state.before),
+      "after": Date.parse(this.state.after),
+      "interval": this.state.interval
+    }
+
+    const query_string =  Object.keys(params).map(key =>
+      key + '=' + params[key]
+    ).join('&');
+
+    return '/results?' + query_string
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    this.setState({ status: 'FETCHING' });
+
+    const self = this;
+
+    fetch(self.resultsPath())
+      .then(response => response.json())
+      .then(json => self.groupByMedium(json))
+      .then(groupedByMedium => self.buildChartData(groupedByMedium))
+      .then(chartData => {
+        self.setState({
+          data: chartData,
+          status: 'FETCHED'
+        });
+      }).catch(function () {
+        self.setState({ status: 'FAILED' });
+      });
+  }
+
+  renderForm () {
+    return(
+      <React.Fragment>
+        <form onSubmit={this.handleSubmit}>
+          Query: <input type="text" name="query" placeholder="Scomo" onChange={this.handleChange} />
+          After: <input type="date" min="2019-08-01" max="2019-08-31" name="after" onChange={this.handleChange} />
+          Before: <input type="date" min="2019-08-01" max="2019-08-31" name="before" onChange={this.handleChange} />
+          Interval: <input type="text" name="interval" placeholder="1d" onChange={this.handleChange} />
+          <button>Search!!!</button>
+        </form>
+      </React.Fragment>
+    )
   }
 
   render () {
-    return (
-      <React.Fragment>
-        <div class="test">Chart: {this.props.data}</div>
-        <ColumnChart stacked={true} data={this.data()} />
-      </React.Fragment>
-    );
+    if (this.state.status === 'FAILED') {
+      return(
+        <React.Fragment>
+          {this.renderForm()}
+          Please review the arguments and try it again.
+        </React.Fragment>
+      )
+    } else if (this.state.status === 'FETCHING') {
+      return(
+        <React.Fragment>
+          {this.renderForm()}
+          Loading
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <React.Fragment>
+          {this.renderForm()}
+          <ColumnChart stacked={true} data={this.state.data} />
+        </React.Fragment>
+      );
+    }
   }
 }
 
--- spec/javascript/components/chart.spec.js
+++ spec/javascript/components/chart.spec.js
@@ -3,39 +3,101 @@ import {shallow} from 'enzyme';
 import Chart from 'components/Chart'
 
 describe('Chart component', () => {
-  describe('data', () => {
-    it('returns the parsed data', () => {
+  describe('groupByMedium', () => {
+    it('group a JSON result by medium', () => {
+      const chart = shallow(<Chart />);
+      const instance = chart.instance();
+      const result = {
+        "aggregations": {
+          "first_agg": {
+            "buckets": [
+              {
+                "sub_agg": {
+                  "buckets": [
+                    {
+                      "key": "Online",
+                      "doc_count": 921
+                    },
+                    {
+                      "key": "TV",
+                      "doc_count": 721
+                    }
+                  ]
+                },
+                "doc_count": 1642,
+                "key_as_string": "2019-08-04T00:00"
+              },
+              {
+                "sub_agg": {
+                  "buckets": [
+                    {
+                      "key": "Online",
+                      "doc_count": 342
+                    },
+                    {
+                      "key": "TV",
+                      "doc_count": 721
+                    }
+                  ]
+                },
+                "doc_count": 1063,
+                "key_as_string": "2019-08-05T00:00"
+              }
+            ]
+          }
+        }
+      };
+
+      const expectedResult = {
+        "Online": [["2019-08-04T00:00", 921], ["2019-08-05T00:00", 342]],
+        "TV": [["2019-08-04T00:00", 721], ["2019-08-05T00:00", 721]]
+      };
+
+      expect(instance.groupByMedium(result)).toEqual(expectedResult);
+    })
+  })
+
+  describe('buildChartData', () => {
+    it('parses the groupByMedium data into chart.js params', () => {
       const chart = shallow(<Chart />);
       const instance = chart.instance();
 
-      expect(instance.data()).toEqual(
-        [
-          {
-            "data": [
-              ["time_stamp_1", 10],
-              ["time_stamp_2", 12],
-              ["time_stamp_3", 15]
-            ],
-            "name": "TV"
-          },
-          {
-            "data": [
-              ["time_stamp_1", 2],
-              ["time_stamp_2", 4],
-              ["time_stamp_3", 12]
-            ],
-            "name": "Radio"
-          },
-          {
-            "data": [
-              ["time_stamp_1", 20],
-              ["time_stamp_2", 23],
-              ["time_stamp_3", 29]
-            ],
-            "name": "Online"
-          }
-        ]
+      const groupedByMedium = {
+        "Online": [["2019-08-04T00:00", 921], ["2019-08-05T00:00", 342]],
+        "TV": [["2019-08-04T00:00", 721], ["2019-08-05T00:00", 721]]
+      }
+
+      const expectedResult = [
+        {
+          "data": [["2019-08-04T00:00", 921], ["2019-08-05T00:00", 342]],
+          "name": "Online"
+        },
+        {
+          "data": [["2019-08-04T00:00", 721], ["2019-08-05T00:00", 721]],
+          "name": "TV"
+        }
+      ]
+
+      expect(instance.buildChartData(groupedByMedium)).toEqual(expectedResult);
+    })
+  })
+
+  describe('resultsPath', () => {
+    it('returns the HTTP path with querystring and unixdate args', () => {
+      const chart = shallow(<Chart />);
+      const instance = chart.instance();
+
+      instance.setState(
+        {
+          query: "Scott",
+          before: "2019-08-01",
+          after: "2019-08-31",
+          interval: "1d",
+        }
       );
+
+      const expected = "/results?query=Scott&before=1564617600000&after=1567209600000&interval=1d";
+      expect(instance.resultsPath()).toEqual(expected);
     })
   })
 })
